4b48048d82d1a06158f406d3f43bd69ef3516dcc
Libero Passador Neto <liberopneto@gmail.com>
2022-12-17 07:44:10 -0300

    Aula 5 • Finalizando o front-end

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { UserProvider } from '@auth0/nextjs-auth0/client';
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
   );
 }
+
+export default MyApp;
